611e75958eb24158f38bda4097c72c6566591c54
nmerolle <nmerolle11@hotmail.com>
2021-07-04 03:39:35 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+let thanks = [] 
+function writeCards(names, string) {
+  for (let i = 0; i < names.length; i++) {
+    thanks.push(`Thank you, ${names[i]}, for the wonderful surprise gift!` );
+  }
+
+  return thanks;
+}
+
+function countDown(number) {
+  while (number>= 0){
+    console.log(number--)
+  }
+}
